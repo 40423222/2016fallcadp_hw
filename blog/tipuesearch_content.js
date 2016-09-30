0603000d4f3721e535001b315e69f19071beaba0
40423222 <40423222@gm.edn.nfu.tw>
2016-09-30 12:20:34 +0800

    add blog and revral data

---
+++ blog/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages":[{"tags":"misc","url":"./pages/about/","text":"2016Fall 修課成員網誌","title":"About"},{"tags":"Misc","url":"./2016fall-jian-bao-yu-wang-zhi-xi-tong.html","text":"這個系統共集結了 reveal.js 網際簡報與 pelican 靜態網誌系統. 網誌 Category 網誌 Tags reveal.js 使用導引","title":"2016Fall 簡報與網誌系統"}]};
